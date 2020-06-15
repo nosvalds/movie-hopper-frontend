09bf175c7183c4bb7036c6919e5a8ad276f2f3cd
Nik Osvalds <nosvalds@gmail.com>
2020-06-15 13:04:22 +0100

    fix searchbutton missing index

---
+++ src/components/SearchButton/index.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import SearchButton from './SearchButton';
+
+const mapStateToProps = (state) => {
+  return {
+  }
+}
+
+export default connect(mapStateToProps)(SearchButton);
