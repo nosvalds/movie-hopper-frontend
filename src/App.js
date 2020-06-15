c6d1bf1d5401ed614b3e9e9d6febe61783851c5d
Nik Osvalds <nosvalds@gmail.com>
2020-06-15 13:06:08 +0100

    move Provider wrapper to index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-
-import { Provider } from "react-redux";
-import store from './data/store';
-
 import Header from './components/Header';
 import MovieGoersList from './components/MovieGoersList/';
 import SearchButton from './components/SearchButton';
@@ -12,12 +8,12 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-   <Provider store={ store }>
+    <>
       <Header />
       <MovieGoersList />
       <SearchButton />
       <Footer />
-    </Provider>
+    </>
    );
 }
 
