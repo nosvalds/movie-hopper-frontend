0c6e24d44c3622d53d83ac76831dddef22f1aef8
Nik Osvalds <nosvalds@gmail.com>
2020-06-15 14:35:04 +0100

    set up reducer actions for MVP

--- src/data/initialState.js
+++ src/data/initialState.js
@@ -1,5 +1,5 @@
 const initialState = {
-  selectedMovieGoer: {},
+  selectedMovieGoer: [],
   movieGoersList:
     [
       {
--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,8 +1,27 @@
-import initialState from './initialState';
+// saves the array of moviegoer people objects to state
+const getMovieGoers = (state, action) => {
+    return {
+        ...state,
+        movieGoersList: action.people, // action.people is an array of person objects passed from API
+    }
+}
 
+const getOneMovieGoer = (state, action) => {
+    return {
+        ...state,
+        selectedMovieGoer: state.movieGoersList.filter((movieGoer) => {
+            return movieGoer.id === action.id; 
+        }),
+        movies: action.movies // action.movies is an array of movies passed from API
+    }
+}
+
+// Reducer function
 const reducer = (state, action) => {
     switch (action.type) {
-      default: return state;
+        case "GET_MOVIEGOERS": return getMovieGoers(state, action);
+        case "GET_ONE_MOVIEGOER": return getOneMovieGoer(state, action);
+        default: return state;
     }
   }
   
