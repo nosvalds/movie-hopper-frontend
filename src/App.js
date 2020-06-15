4d016ea187b5a9867bb66ad43fa18f3a76e5b94a
Nik Osvalds <nosvalds@gmail.com>
2020-06-15 12:57:23 +0100

    move provider to index and fix searchbutton export issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import './App.css';
-
-import { Provider } from "react-redux";
-import store from './data/store';
-
 import Header from './components/Header';
-import MovieGoersList from './components/MovieGoersList/';
+import MovieGoersList from './components/MovieGoersList';
 import SearchButton from './components/SearchButton';
 import Footer from './components/Footer';
 
 
-function App() {
+const App = () => {
   return (
-   <Provider store={ store }>
+    <>
       <Header />
       <MovieGoersList />
       <SearchButton />
       <Footer />
-    </Provider>
+    </>
    );
 }
 
