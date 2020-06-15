98f6ac951601bbdc5909cee9bdb22ac453bb142c
Nik Osvalds <nosvalds@gmail.com>
2020-06-15 12:23:48 +0100

    add store and reducer, set up provider to show initial state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Provider } from "react-redux";
+import store from './data/store';
 import MovieGoersList from './components/MovieGoersList/';
 
 function App() {
   return (
-    <>
+    <Provider store={ store }>
       <MovieGoersList />
-    </>
+    </Provider>
   );
 }
 
---
+++ src/data/reducer.js
@@ -0,0 +1,9 @@
+import initialState from './initialState';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+      default: return state;
+    }
+  }
+  
+  export default reducer;
