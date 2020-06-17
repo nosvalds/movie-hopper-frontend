c5ea47d4425ff8002c0498ff457e99a819889f90
Nik Osvalds <nosvalds@gmail.com>
2020-06-17 12:15:46 +0100

    adding some color

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
 
   <Jumbotron>
     <h1 className="moviegoers-title display-4">Movie Hopper</h1>
-    <p class="lead text-center">Select a person to see their favourite movies. Select multiple to see which favourite films they have in common.</p>
+    <p className="lead text-center">Select a person to see their favourite movies. Select multiple to see which favourite films they have in common.</p>
   </Jumbotron >
 )
 
