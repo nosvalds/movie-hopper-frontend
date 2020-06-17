d352e717182df46dec4ada4d96862d4a4abcb52f
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-17 11:26:56 +0100

    Updates

---
+++ precache-manifest.5e1732ee61f6f9609e0b1a94c506e232.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f9bd07b4065d20f981d0d84e8a77cd",
+    "url": "/movie-hopper-frontend/index.html"
+  },
+  {
+    "revision": "5dd2022ec2d3612cd05e",
+    "url": "/movie-hopper-frontend/static/css/main.62e947dd.chunk.css"
+  },
+  {
+    "revision": "fdb41acf0828e419ca78",
+    "url": "/movie-hopper-frontend/static/js/2.41a0e26e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-hopper-frontend/static/js/2.41a0e26e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd2022ec2d3612cd05e",
+    "url": "/movie-hopper-frontend/static/js/main.2b1436a7.chunk.js"
+  },
+  {
+    "revision": "4005439ccf9e96a12947",
+    "url": "/movie-hopper-frontend/static/js/runtime-main.91829d85.js"
+  }
+]);
