5c86a1d84f3e3325f54d30a23b4656e6ae760a62
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-17 10:19:39 +0100

    Reset button working

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,41 +1,42 @@
 
 const selectMovieGoer = (state, action) => {
-    return {
-        ...state,
-        selectedMovieGoer: [
-            ...state.selectedMovieGoer]
-            .concat(state.movieGoersList.filter((movieGoer) => {
-                return movieGoer.id === action.id;
-            })),
-    }
+  return {
+    ...state,
+    selectedMovieGoer: [
+      ...state.selectedMovieGoer]
+      .concat(state.movieGoersList.filter((movieGoer) => {
+        return movieGoer.id === action.id;
+      })),
+  }
 }
 
 const setPeople = (state, action) => {
-    return {
-        ...state,
-        movieGoersList: action.data
-    }
+  return {
+    ...state,
+    movieGoersList: action.data
+  }
 }
 
 const setMovies = (state, action) => {
-    return {
-        ...state,
-        movies: action.movies,
-    }
+  return {
+    ...state,
+    movies: action.movies,
+  }
 }
 
 // Reducer function
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "SET_PEOPLE": return setPeople(state, action);
-        case "SELECT_MOVIEGOER": return selectMovieGoer(state, action);
-        case "SET_MOVIES": return setMovies(state, action);
-        case "CLEAR_PEOPLE": return { // clear selected moviegoers
-            ...state,
-            selectedMovieGoer: [],
-        };
-        default: return state;
-    }
+  switch (action.type) {
+    case "SET_PEOPLE": return setPeople(state, action);
+    case "SELECT_MOVIEGOER": return selectMovieGoer(state, action);
+    case "SET_MOVIES": return setMovies(state, action);
+    case "CLEAR_ALL": return { // clear selected moviegoers
+      ...state,
+      selectedMovieGoer: [],
+      movies: []
+    };
+    default: return state;
+  }
 }
 
 export default reducer;
