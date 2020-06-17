9d1e09cc9805469812d581de52e45bba38d8271b
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-17 15:04:38 +0100

    updated base url

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: "https://hopper.developme.space/",
+  baseURL: "https://hopper.developme.space/api/",
   headers: {
     Accept: "application/json",
     // Authorization: "TBD" // Needs to be set up
