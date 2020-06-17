2c824159a3a4c027effa6e746835078e6de304e2
rmstibbard <rmstibbard@users.noreply.github.com>
2020-06-17 21:06:16 +0100

    Further styling corrections

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,11 @@ import { Jumbotron } from 'react-bootstrap';
 const Header = () => (
 
   <Jumbotron className="header">
-    <h1 className="moviegoers-title display-4">Movie Hopper</h1>
-    <p className="lead text-center header-para">Select a person to see their favourite movies. Select multiple to see which favourite films they have in common.</p>
+    <h1 className="header-h1">Movie Hopper</h1>
+    <p className="header-para">
+      Select a person to see their favourite movies.<br />
+      Select multiple to see films they have in common.
+    </p>
   </Jumbotron >
 )
 
