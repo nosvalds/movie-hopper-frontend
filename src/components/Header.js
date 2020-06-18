1747cd335e505fd7730d71bfca69a5865f519800
Nik Osvalds <nosvalds@gmail.com>
2020-06-18 15:56:04 +0100

    make header take you home

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 const Header = () => (
-
-  <Jumbotron className="header">
-    <h1 className="header-h1" aria-label="Movie Hopper App - Find Movies in Common!">Movie Hopper</h1>
-  </Jumbotron >
+  <a href="/">
+    <Jumbotron className="header">
+        <h1 className="header-h1" aria-label="Movie Hopper App - Find Movies in Common!">Movie Hopper</h1>
+    </Jumbotron >
+  </a>
 )
 
 export default Header;
