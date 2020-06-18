672adbe7f4b42171aeb03c7eec89b3de9717d26c
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-18 10:06:16 +0100

    deselection of people complete

--- src/data/actions/api.js
+++ src/data/actions/api.js
@@ -1,8 +1,9 @@
 import axios from "../../axios/axios"
-import { selectMovieGoer,
-         setPeople,
-         setMovies,   
-        } from './state'
+import {
+    selectMovieGoer,
+    setPeople,
+    setMovies,
+} from './state'
 
 export const getPeople = () => {
     return (dispatch) => {
@@ -13,25 +14,31 @@ export const getPeople = () => {
 }
 
 export const getMovies = (id) => (dispatch, getState) => {
-    let peopleIds = [];
+    dispatch(selectMovieGoer(id))
     let selectedMovieGoers = getState().selectedMovieGoer;
-    if (selectedMovieGoers.length === 0) {
-        peopleIds[0] = id;
-    } else {
-        peopleIds = selectedMovieGoers.map((person) => person.id);
-        peopleIds.push(id); // array of currently selected movie-goers
-    }
+    let peopleIds = selectedMovieGoers.map(person => person.id);
+    console.log(peopleIds)
+    // if (selectedMovieGoers.length === 0) {
+    //     peopleIds[0] = id;
+    // } else {
+    //     peopleIds = selectedMovieGoers.map((person) => person.id);
+    //     peopleIds.push(id); // array of currently selected movie-goers
+    // }
     // if it's 1 selection query the /people/id route
     if (peopleIds.length === 1) {
-        axios.get(`/people/${id}`).then(({ data }) => { 
+        console.log("In if")
+        axios.get(`/people/${id}`).then(({ data }) => {
             dispatch(setMovies(data.data.movies))
+
         })
-    } else {
+    } else if (peopleIds.length > 1) {
+        console.log("In else")
         // if it's multiple selections, query the match?people="ids" route
-       axios.get(`/people/match?people=${peopleIds.join(',')}`).then(({ data }) => {
+        axios.get(`/people/match?people=${peopleIds.join(',')}`).then(({ data }) => {
             dispatch(setMovies([data.data]))
-       });
+        });
     }
+    console.log(peopleIds.length)
     // always "select" a moviegoer, could add error handling if the above api calls failed.
-    dispatch(selectMovieGoer(id))
+
 }
