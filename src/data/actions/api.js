ec0099d9cf2b8c4125e03900b7a649f0a22aca5c
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-18 12:23:04 +0100

    deselection updates and accessibility changes

--- src/data/actions/api.js
+++ src/data/actions/api.js
@@ -17,7 +17,6 @@ export const getMovies = (id) => (dispatch, getState) => {
     dispatch(selectMovieGoer(id))
     let selectedMovieGoers = getState().selectedMovieGoer;
     let peopleIds = selectedMovieGoers.map(person => person.id);
-    console.log(peopleIds)
     // if (selectedMovieGoers.length === 0) {
     //     peopleIds[0] = id;
     // } else {
@@ -26,19 +25,16 @@ export const getMovies = (id) => (dispatch, getState) => {
     // }
     // if it's 1 selection query the /people/id route
     if (peopleIds.length === 1) {
-        console.log("In if")
         axios.get(`/people/${id}`).then(({ data }) => {
             dispatch(setMovies(data.data.movies))
 
         })
     } else if (peopleIds.length > 1) {
-        console.log("In else")
         // if it's multiple selections, query the match?people="ids" route
         axios.get(`/people/match?people=${peopleIds.join(',')}`).then(({ data }) => {
             dispatch(setMovies([data.data]))
         });
     }
-    console.log(peopleIds.length)
     // always "select" a moviegoer, could add error handling if the above api calls failed.
 
 }
