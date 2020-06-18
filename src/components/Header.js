9a677ae84016203410bf4b235b42872a0df270c1
Nik Osvalds <nosvalds@gmail.com>
2020-06-18 17:32:31 +0100

    fix header link for hash

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 const Header = () => (
-  <a className="h1-link" href="/">
+  <a className="h1-link" href="#/">
     <Jumbotron className="header">
       <h1 className="header-h1" aria-label="Movie Hopper App - Find Movies in Common!">Movie Hopper</h1>
     </Jumbotron >
