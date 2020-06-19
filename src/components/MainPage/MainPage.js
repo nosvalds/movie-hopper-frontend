9254d897f67fe145ab8a4f7cbdf86928f1762783
Nik Osvalds <nosvalds@gmail.com>
2020-06-19 09:27:39 +0100

    add container around body, button hover text

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -4,15 +4,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilm, faUserFriends } from '@fortawesome/free-solid-svg-icons'
 
 const MainPage = () => (
-    <section className="container mt-4 p-4">
-        <a href="#/movies" className="m-2">
-            <Button aria-label="start with films" className="p-2" variant="primary" size="lg" block>
-                <FontAwesomeIcon icon={ faFilm } size="6x" aria-label="film reel icon" />
+    <section className="container mt-4 p-4 big-buttons-container">
+        <a href="#/movies" className="m-4">
+            <Button id="movies-btn" aria-label="start with films" className="p-2" variant="primary" size="lg" block>
+                <span class="hover-hide"><FontAwesomeIcon icon={ faFilm } size="6x" aria-label="film reel icon" /></span>
             </Button>
         </a>
-        <a href="#/people" className="m-2">
-            <Button aria-label="start with people" className="p-2" variant="secondary" size="lg" block>
-                <FontAwesomeIcon icon={ faUserFriends } size="6x" aria-label="people icon"/>
+        <a href="#/people" className="m-4">
+            <Button id="people-btn" aria-label="start with people" className="p-2" variant="secondary" size="lg" block>
+            <span class="hover-hide"><FontAwesomeIcon icon={ faUserFriends } size="6x" aria-label="people icon"/></span>
             </Button>
         </a>
     </section>
